a5a2613c4d5e08b0539e66d123e311e2ad751fe7
Zuxy <85226854+ZuxyGanz@users.noreply.github.com>
2021-08-15 17:29:58 +0700

    Nih

--- start.js
+++ start.js
@@ -1,9 +1,13 @@
 
 			
 						
-   Let Ziyy('slow My Skill')
+   <script>slow My Skill</script>
    const skill = ALDI('HTML, java, nodejs')
    status MySkill(new, skil)
+   try{
    info: 'skill',
    HTML: '50%',
-	   
+   Java: '70%',
+   Nodejs: '90%',
+   Handler: '0%',
+   }
