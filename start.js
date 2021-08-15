99b431db2a72a6297a68b24031595a086b91ad19
Zuxy <85226854+ZuxyGanz@users.noreply.github.com>
2021-08-15 17:33:54 +0700

    Aha

--- start.js
+++ start.js
@@ -2,12 +2,11 @@
 			
 						
    if respon skil = slow My Skill
-   const skill = ALDI(HTML, java, nodejs, handler)
+   const skill = ALDI(HTML, java, nodejs)
    status MySkill(new, skil)
    try{
    info: 'skill',
    HTML: '50%',
    Java: '70%',
-   Nodejs: '90%',
-   Handler: '0%',
+   Nodejs: '90%'
    }
